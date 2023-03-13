d17c7cb29dd4dcf9b0ff3f98538ff95e9193816f
kelseyabbey <kelseyabbey2@gmail.com>
2023-03-13 11:55:40 -0600

    all scripts/text files updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,72 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import os
+import csv
+
+#read csv
+file = os.path.join(r"/Users/kelseyabbey/Documents/Data Analytics/Module 3 Challenge/python-challenge/PyBank/Resources/budget_data.csv")
+#import csv file as dataframe
+pybank = pd.read_csv(file, encoding = "utf8")
+
+#headers: profit/loss & dates
+profit_loss = pybank["Profit/Losses"]
+dates = pybank["Date"]
+
+#total months
+total_months = len(pybank)
+
+#net total
+net_total = pybank["Profit/Losses"].sum()
+
+#average change
+average_change = round(((profit_loss[total_months - 1]- profit_loss[0]) / (total_months - 1)),2)
+
+#set variables
+greatest_increase = 0
+greatest_decrease = 0
+
+for index,nums in enumerate(profit_loss):
+
+    if index < total_months - 1:
+        #month to month difference
+        difference = profit_loss[index + 1] - profit_loss[index]
+
+        #greatest increase/decrease with month and amount
+        if difference > greatest_increase:
+            greatest_increase = difference
+            date_on_increase = dates[index + 1]
+
+        elif difference < greatest_decrease:
+            greatest_decrease = difference
+            date_on_decrease = dates[index + 1]
+
+    #for last index
+    else:
+        difference = profit_loss[index] - profit_loss[index - 1]
+
+        if difference < greatest_decrease:
+            greatest_decrease = difference
+            date_on_decrease = dates[index + 1]
+        elif difference > greatest_increase:
+            greatest_increase = difference
+            date_on_increase = dates[index + 1]
+
+
+#output to text file for both read/write
+output = os.path.join(r"/Users/kelseyabbey/Documents/Data Analytics/Module 3 Challenge/python-challenge/PyBank/Analysis/Financial_Analysis.txt")
+with open(output,"w+") as text:
+    text.write(f"\nFinancial Analysis \n---------------\n")
+    text.write(f"Total Months: {total_months}\n")
+    text.write(f"Net Total: ${net_total:,}\n")
+    text.write(f"Average Change:: ${average_change:,}\n")
+    text.write(f"Greatest Increase: {date_on_increase} (${greatest_increase:,})\n")
+    text.write(f"Greatest Decrease: {date_on_decrease} (${greatest_decrease:,})\n---------------\n")
+
+#print output to terminal
+    print(f"Financial Analysis")
+    print("---------------")
+    print(f"Total Months: {total_months}")
+    print(f"Net Total: ${net_total:,}")
+    print(f"Average Change:: ${average_change:,}")
+    print(f"Greatest Increase: {date_on_increase} (${greatest_increase:,})")
+    print(f"Greatest Decrease: {date_on_decrease} (${greatest_decrease:,})")
+    print("---------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,68 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import os
+import csv
+
+#read csv
+file = os.path.join(r"/Users/kelseyabbey/Documents/Data Analytics/Module 3 Challenge/python-challenge/PyPoll/Resources/election_data.csv")
+#import csv file as dataframe
+pypoll = pd.read_csv(file, encoding = "utf8")
+
+#total votes
+total_votes = len(pypoll)
+
+#candidate names and counters
+candidate1 = "Charles Casper Stockham"
+candidate2 = "Diana DeGette"
+candidate3 = "Raymon Anthony Doane"
+charlescount = 0
+dianacount = 0
+raymoncount = 0
+
+#variable for vote count
+vote_count = pypoll["Ballot ID"]
+
+#loop to add to vote count
+for index, row in enumerate(vote_count):
+    if pypoll["Candidate"][index] == candidate1:
+        charlescount += 1
+    elif pypoll["Candidate"][index] == candidate2:
+        dianacount += 1
+    elif pypoll["Candidate"][index] == candidate3:
+        raymoncount += 1
+
+#percentage votes per candidate
+candidate1_percent = round((charlescount/total_votes) * 100, 3)
+candidate2_percent = round((dianacount/total_votes) * 100, 3)
+candidate3_percent = round((raymoncount/total_votes) * 100, 3)
+
+#winner
+if charlescount > dianacount and charlescount > raymoncount:
+    winner = candidate1
+elif dianacount > charlescount and dianacount > raymoncount:
+    winner = candidate2
+elif raymoncount > charlescount and raymoncount > dianacount:
+    winner = candidate3
+else:
+    winner = "There's a tie!"
+
+#output to text file for both read/write
+output = os.path.join(r"/Users/kelseyabbey/Documents/Data Analytics/Module 3 Challenge/python-challenge/PyPoll/Analysis/Election_Results.txt")
+with open(output,"w+") as text:
+    text.write(f"\nElection Results \n---------------\n")
+    text.write(f"Total Votes: {total_votes}\n---------------\n")
+    text.write(f"{candidate1}: {candidate1_percent}% ({charlescount})\n")
+    text.write(f"{candidate2}: {candidate2_percent}% ({dianacount})\n")
+    text.write(f"{candidate3}: {candidate3_percent}% ({raymoncount})\n---------------\n")
+    text.write(f"Winner: {winner}\n---------------\n")
+
+#print output to terminal
+    print(f"Election Results")
+    print("---------------")
+    print(f"Total Votes: {total_votes}")
+    print("---------------")
+    print(f"{candidate1}: {candidate1_percent}% ({charlescount})")
+    print(f"{candidate2}: {candidate2_percent}% ({dianacount})")
+    print(f"{candidate3}: {candidate3_percent}% ({raymoncount})")
+    print("---------------")
+    print(f"Winner: {winner}")
+    print("---------------")
